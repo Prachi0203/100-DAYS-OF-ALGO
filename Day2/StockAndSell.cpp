a57111ac4b4b8544702057eb4604d9ea3370379d
Prachi0203 <58703496+Prachi0203@users.noreply.github.com>
2020-09-20 16:09:08 +0530

    Update StockAndSell.cpp
    The solution is based on finding the local minima and local maxima.

--- Day2/StockAndSell.cpp
+++ Day2/StockAndSell.cpp
@@ -1,11 +1,29 @@
-//stock and sell maximum profit...........
+//stock and sell maximum profit...........using local minmima ad local maxima.......
 #include<iostream>
 using namespace std;
-int maxprofit(int a,int l,int h){
-	
+void maxprofit(int a[],int n){
+	int i = 0;
+	if(n==1){
+		return;
+	}
+	while(i<n-1){
+		while((i<n-1)&&(a[i+1]<=a[i])){
+			i++;
+		}
+		if(i==n-1)
+		break;
+	int	buy = i++;
+		while(i<n&&(a[i]>=a[i-1])){
+		  i++;
+		}
+	int	sell = i-1;
+			//for(int i=0;i<count;i++){
+		cout<<"stock buy on day: "<<buy<<" stock sell on day: "<<sell<<"\t";
+	}
+
 }
 int main(){
-	int a[]={1,5,3,8,12};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	maxprofit(a,n);
+	int a[]={100,180,260,310,40,535,695};
+	int n = sizeof(a)/sizeof(a[0]);
+    maxprofit(a,n);
 }
